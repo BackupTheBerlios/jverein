5616d8c406cc8cf8f8d6169497f2492b3983f19b
jost <jost>
2007-08-31 05:36:00 +0000

    Jetzt auch bearbeiten über das Context-Menü

--- jverein/src/de/jost_net/JVerein/gui/menu/MitgliedMenu.java
+++ jverein/src/de/jost_net/JVerein/gui/menu/MitgliedMenu.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/menu/MitgliedMenu.java,v $
- * $Revision: 1.1 $
- * $Date: 2007/08/30 19:48:53 $
+ * $Revision: 1.2 $
+ * $Date: 2007/08/31 05:36:00 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: MitgliedMenu.java,v $
+ * Revision 1.2  2007/08/31 05:36:00  jost
+ * Jetzt auch bearbeiten über das Context-Menü
+ *
  * Revision 1.1  2007/08/30 19:48:53  jost
  * Neues Kontext-Menü
  *
@@ -16,6 +19,7 @@
 package de.jost_net.JVerein.gui.menu;
 
 import de.jost_net.JVerein.gui.action.MitgliedDeleteAction;
+import de.jost_net.JVerein.gui.action.MitgliedDetailAction;
 import de.willuhn.jameica.gui.parts.CheckedContextMenuItem;
 import de.willuhn.jameica.gui.parts.ContextMenu;
 
@@ -30,6 +34,7 @@ public class MitgliedMenu extends ContextMenu
    */
   public MitgliedMenu()
   {
+    addItem(new CheckedContextMenuItem("Bearbeiten", new MitgliedDetailAction()));
     addItem(new CheckedContextMenuItem("L�schen...", new MitgliedDeleteAction()));
   }
 }
