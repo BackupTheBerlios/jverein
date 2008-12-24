c98eac710bb81fed60f6ff42fc3e0f06d8dd8169
jost <jost>
2008-12-24 09:17:09 +0000

    Bei Altersjubiläen wird jetzt das Geburtsdatum anstatt des Eintrittsdatums ausgegeben.

--- jverein/src/de/jost_net/JVerein/io/Jubilaeenliste.java
+++ jverein/src/de/jost_net/JVerein/io/Jubilaeenliste.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/io/Jubilaeenliste.java,v $
- * $Revision: 1.5 $
- * $Date: 2008/12/06 16:46:57 $
+ * $Revision: 1.6 $
+ * $Date: 2008/12/24 09:17:09 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: Jubilaeenliste.java,v $
+ * Revision 1.6  2008/12/24 09:17:09  jost
+ * Bei Altersjubiläen wird jetzt das Geburtsdatum anstatt des Eintrittsdatums ausgegeben.
+ *
  * Revision 1.5  2008/12/06 16:46:57  jost
  * Debug-Meldung entfernt.
  *
@@ -142,7 +145,7 @@ public class Jubilaeenliste
           FontFactory.getFont(FontFactory.HELVETICA, 11));
       reporter.add(pHeader);
 
-      addHeader(reporter);
+      addHeader(MitgliedControl.JUBELART_MITGLIEDSCHAFT, reporter);
 
       DBIterator mitgl = Einstellungen.getDBService()
           .createList(Mitglied.class);
@@ -166,7 +169,7 @@ public class Jubilaeenliste
       while (mitgl.hasNext())
       {
         Mitglied m = (Mitglied) mitgl.next();
-        addDetail(reporter, m);
+        addDetail(MitgliedControl.JUBELART_MITGLIEDSCHAFT, reporter, m);
       }
       if (mitgl.size() == 0)
       {
@@ -191,7 +194,7 @@ public class Jubilaeenliste
           FontFactory.getFont(FontFactory.HELVETICA, 11));
       reporter.add(pHeader);
 
-      addHeader(reporter);
+      addHeader(MitgliedControl.JUBELART_ALTER, reporter);
 
       DBIterator mitgl = Einstellungen.getDBService()
           .createList(Mitglied.class);
@@ -215,7 +218,7 @@ public class Jubilaeenliste
       while (mitgl.hasNext())
       {
         Mitglied m = (Mitglied) mitgl.next();
-        addDetail(reporter, m);
+        addDetail(MitgliedControl.JUBELART_ALTER, reporter, m);
       }
       if (mitgl.size() == 0)
       {
@@ -229,10 +232,20 @@ public class Jubilaeenliste
 
   }
 
-  private void addHeader(Reporter reporter) throws DocumentException
+  private void addHeader(String art, Reporter reporter)
+      throws DocumentException
   {
-    reporter.addHeaderColumn("Eintrittsdatum", Element.ALIGN_CENTER, 50,
-        Color.LIGHT_GRAY);
+    if (art.equals(MitgliedControl.JUBELART_MITGLIEDSCHAFT))
+    {
+      reporter.addHeaderColumn("Eintrittsdatum", Element.ALIGN_CENTER, 50,
+          Color.LIGHT_GRAY);
+    }
+    if (art.equals(MitgliedControl.JUBELART_ALTER))
+    {
+      reporter.addHeaderColumn("Geburtsdatum", Element.ALIGN_CENTER, 50,
+          Color.LIGHT_GRAY);
+    }
+
     reporter.addHeaderColumn("Name, Vorname", Element.ALIGN_CENTER, 100,
         Color.LIGHT_GRAY);
     reporter.addHeaderColumn("Anschrift", Element.ALIGN_CENTER, 120,
@@ -243,9 +256,17 @@ public class Jubilaeenliste
 
   }
 
-  private void addDetail(Reporter reporter, Mitglied m) throws RemoteException
+  private void addDetail(String art, Reporter reporter, Mitglied m)
+      throws RemoteException
   {
-    reporter.addColumn(m.getEintritt(), Element.ALIGN_LEFT);
+    if (art.equals(MitgliedControl.JUBELART_MITGLIEDSCHAFT))
+    {
+      reporter.addColumn(m.getEintritt(), Element.ALIGN_LEFT);
+    }
+    if (art.equals(MitgliedControl.JUBELART_ALTER))
+    {
+      reporter.addColumn(m.getGeburtsdatum(), Element.ALIGN_LEFT);
+    }
     reporter.addColumn(m.getNameVorname(), Element.ALIGN_LEFT);
     reporter.addColumn(m.getAnschrift(), Element.ALIGN_LEFT);
     String kommunikation = m.getTelefonprivat();
