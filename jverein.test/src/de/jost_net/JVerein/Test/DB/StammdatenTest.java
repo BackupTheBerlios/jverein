0409a78211a272fc7bc6155b17ddf0bc28740db8
jost <jost>
2009-07-01 07:19:49 +0000

    Jameica-Default-Logger verwenden

--- jverein.test/src/de/jost_net/JVerein/Test/DB/StammdatenTest.java
+++ jverein.test/src/de/jost_net/JVerein/Test/DB/StammdatenTest.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein.test/src/de/jost_net/JVerein/Test/DB/Attic/StammdatenTest.java,v $
- * $Revision: 1.1 $
- * $Date: 2009/07/01 07:02:20 $
+ * $Revision: 1.2 $
+ * $Date: 2009/07/01 07:19:49 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,25 +9,30 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: StammdatenTest.java,v $
+ * Revision 1.2  2009/07/01 07:19:49  jost
+ * Jameica-Default-Logger verwenden
+ *
  * Revision 1.1  2009/07/01 07:02:20  jost
  * Initial
  *
  **********************************************************************/
 package de.jost_net.JVerein.Test.DB;
 
-import java.util.logging.Logger;
-
 import de.jost_net.JVerein.Test.AbstractTest;
 import de.jost_net.JVerein.rmi.Stammdaten;
 import de.willuhn.datasource.rmi.DBIterator;
+import de.willuhn.logging.Logger;
 
 public class StammdatenTest extends AbstractTest
 {
-  private static Logger logger = Logger.getLogger(Stammdaten.class.getName());
+  public StammdatenTest(String arg0)
+  {
+    super(arg0);
+  }
 
   public void testCreateStammdaten() throws Exception
   {
-    logger.info("Teste Erstellung der Stammdaten");
+    Logger.info("Teste Erstellung der Stammdaten");
     Stammdaten st = (Stammdaten) getDBService().createObject(Stammdaten.class,
         null);
     st.setName("JVerein");
@@ -36,23 +41,11 @@ public class StammdatenTest extends AbstractTest
     st.setKonto("1234567890");
     st.setAltersjubilaeen("65,70,75,80,85,90,91,92,93,94,95,96,97,98,99,100");
 
-    logger.info("Teste Speichern der Stammdaten");
+    Logger.info("Teste Speichern der Stammdaten");
     st.store();
     assertNotNull(st.getID());
-    logger.info("Teste auf Vorhandensein in Iterator");
+    Logger.info("Teste auf Vorhandensein in Iterator");
     DBIterator i = getDBService().createList(Stammdaten.class);
     assertNotNull(i.contains(st));
-
   }
-
-  /**
-   * Constructor for KontoImplTest.
-   * 
-   * @param arg0
-   */
-  public StammdatenTest(String arg0)
-  {
-    super(arg0);
-  }
-
 }
