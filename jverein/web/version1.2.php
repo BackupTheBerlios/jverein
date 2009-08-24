8bffccddaef4adc960210371497f0d983bd346e2
jost <jost>
2009-08-24 17:38:08 +0000

    Typo

--- jverein/web/version1.2.php
+++ jverein/web/version1.2.php
@@ -7,7 +7,7 @@
 	<li>Die Menüpunkte von Plugins>JVerein ins Hauptmenü kopiert</li>
 	<li>Juristische Personen (Firmen, Organisationen, Behörden) können als Mitglied gespeichert werden.</li>
 	<li>Lehrgänge</li>
-	<li>Zusätzliche Felder zur Rechnungserstelltung.</li>
+	<li>Zusätzliche Felder zur Rechnungserstellung.</li>
 	<li>Silbentrennung in Reports aufgenommen</li>
 	<li>Icons auf Buttons</li>
 	<li>Export der Mitgliederdaten als Adressbuch für div. Mail-Programmme</li>
@@ -15,6 +15,9 @@
 </ul>
     <!-- 
     $Log: version1.2.php,v $
+    Revision 1.2  2009/08/24 17:38:08  jost
+    Typo
+
     Revision 1.1  2009/08/23 18:59:30  jost
     Version 1.2
 
