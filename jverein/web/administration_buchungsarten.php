0a02272b5f86acb1e6cf3b783332b9cd8668eb93
jost <jost>
2009-09-01 19:53:17 +0000

    Diese Nummer dient der Sortierung der Buchungsliste

--- jverein/web/administration_buchungsarten.php
+++ jverein/web/administration_buchungsarten.php
@@ -3,13 +3,16 @@
     <p>
     <img src='images/Buchungsartenuebersicht.jpg' class='screenshot'>
     <p>Mit <b>neu</b> kann eine neue Buchungsart eingerichtet werden. Jeder Buchungsart wird eine
-    Nummer zugeordnet. Diese Nummer dient der Sortierung im Jahresabschluss. Z. B. werden den Einnahmen
+    Nummer zugeordnet. Diese Nummer dient der Sortierung in der Buchungsliste. Z. B. werden den Einnahmen
     die 1.000er Nummern, den Ausgaben die 2.000er und den Umbuchungen die 3.000er gegeben.</p>
     <p>Für jede Buchungsart wird die Art "Einnahme", "Ausgabe" oder "Umbuchung" ausgewählt.</p>
     <img src='images/Buchungsart.jpg' class='screenshot'>
     
     <!-- 
     $Log: administration_buchungsarten.php,v $
+    Revision 1.2  2009/09/01 19:53:17  jost
+    Diese Nummer dient der Sortierung der Buchungsliste
+
     Revision 1.1  2009/08/20 20:15:32  jost
     *** empty log message ***
 
