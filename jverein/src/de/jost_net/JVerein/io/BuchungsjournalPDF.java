0e7dd7d2360e99cef0629b023460f55a362ad4ac
jost <jost>
2009-09-15 19:22:36 +0000

    Summenbildung.

--- jverein/src/de/jost_net/JVerein/io/BuchungsjournalPDF.java
+++ jverein/src/de/jost_net/JVerein/io/BuchungsjournalPDF.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/io/BuchungsjournalPDF.java,v $
- * $Revision: 1.1 $
- * $Date: 2009/09/12 19:04:44 $
+ * $Revision: 1.2 $
+ * $Date: 2009/09/15 19:22:36 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: BuchungsjournalPDF.java,v $
+ * Revision 1.2  2009/09/15 19:22:36  jost
+ * Summenbildung.
+ *
  * Revision 1.1  2009/09/12 19:04:44  jost
  * neu: Buchungsjournal
  *
@@ -27,6 +30,7 @@ import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
 
 import de.jost_net.JVerein.Einstellungen;
+import de.jost_net.JVerein.keys.ArtBuchungsart;
 import de.jost_net.JVerein.rmi.Buchung;
 import de.jost_net.JVerein.rmi.Konto;
 import de.willuhn.datasource.rmi.DBIterator;
@@ -57,6 +61,11 @@ public class BuchungsjournalPDF
       Reporter reporter = new Reporter(fos, monitor, "Buchungsjournal",
           subtitle, list.size());
 
+      double einnahmen = 0;
+      double ausgaben = 0;
+      double umbuchungen = 0;
+      double nichtzugeordnet = 0;
+
       createTableHeader(reporter);
 
       while (list.hasNext())
@@ -65,6 +74,7 @@ public class BuchungsjournalPDF
         DBIterator listk = Einstellungen.getDBService().createList(Konto.class);
         listk.addFilter("id = ?", new Object[] { b.getKonto().getID() });
         Konto k = (Konto) listk.next();
+        reporter.addColumn(b.getID(), Element.ALIGN_RIGHT);
         reporter.addColumn(Einstellungen.DATEFORMAT.format(b.getDatum()),
             Element.ALIGN_LEFT);
         reporter.addColumn(k.getNummer(), Element.ALIGN_RIGHT);
@@ -78,10 +88,76 @@ public class BuchungsjournalPDF
           reporter.addColumn("", Element.ALIGN_LEFT);
         }
         reporter.addColumn(b.getName(), Element.ALIGN_LEFT);
-        reporter.addColumn(b.getZweck(), Element.ALIGN_LEFT);
-        reporter.addColumn(b.getZweck2(), Element.ALIGN_LEFT);
+        reporter.addColumn(b.getZweck()
+            + (b.getZweck2() != null ? (" " + b.getZweck2()) : ""),
+            Element.ALIGN_LEFT);
+        reporter.addColumn(b.getBuchungsart() != null ? b.getBuchungsart()
+            .getBezeichnung() : "", Element.ALIGN_LEFT);
         reporter.addColumn(b.getBetrag());
+        if (b.getBuchungsart() != null)
+        {
+          int buchungsartart = b.getBuchungsart().getArt();
+          switch (buchungsartart)
+          {
+            case ArtBuchungsart.EINNAHME:
+            {
+              einnahmen += b.getBetrag();
+              break;
+            }
+            case ArtBuchungsart.AUSGABE:
+            {
+              ausgaben += b.getBetrag();
+              break;
+            }
+            case ArtBuchungsart.UMBUCHUNG:
+            {
+              umbuchungen += b.getBetrag();
+              break;
+            }
+          }
+        }
+        else
+        {
+          nichtzugeordnet += b.getBetrag();
+        }
+
       }
+
+      for (int i = 0; i < 5; i++)
+      {
+        reporter.addColumn("", Element.ALIGN_LEFT);
+      }
+      reporter.addColumn("Summe Einnahmen", Element.ALIGN_LEFT);
+      reporter.addColumn("", Element.ALIGN_LEFT);
+      reporter.addColumn(einnahmen);
+
+      for (int i = 0; i < 5; i++)
+      {
+        reporter.addColumn("", Element.ALIGN_LEFT);
+      }
+      reporter.addColumn("Summe Ausgaben", Element.ALIGN_LEFT);
+      reporter.addColumn("", Element.ALIGN_LEFT);
+      reporter.addColumn(ausgaben);
+
+      for (int i = 0; i < 5; i++)
+      {
+        reporter.addColumn("", Element.ALIGN_LEFT);
+      }
+      reporter.addColumn("Summe Umbuchungen", Element.ALIGN_LEFT);
+      reporter.addColumn("", Element.ALIGN_LEFT);
+      reporter.addColumn(umbuchungen);
+
+      if (nichtzugeordnet != 0)
+      {
+        for (int i = 0; i < 5; i++)
+        {
+          reporter.addColumn("", Element.ALIGN_LEFT);
+        }
+        reporter.addColumn("Summe nicht zugeordnet", Element.ALIGN_LEFT);
+        reporter.addColumn("", Element.ALIGN_LEFT);
+        reporter.addColumn(nichtzugeordnet);
+      }
+
       reporter.closeTable();
       monitor.setStatusText("Auswertung fertig. " + list.size() + " Sätze.");
 
@@ -125,6 +201,7 @@ public class BuchungsjournalPDF
 
   private void createTableHeader(Reporter reporter) throws DocumentException
   {
+    reporter.addHeaderColumn("Nr", Element.ALIGN_RIGHT, 20, Color.LIGHT_GRAY);
     reporter.addHeaderColumn("Datum", Element.ALIGN_CENTER, 45,
         Color.LIGHT_GRAY);
     reporter.addHeaderColumn("Konto", Element.ALIGN_CENTER, 40,
@@ -135,7 +212,7 @@ public class BuchungsjournalPDF
         Color.LIGHT_GRAY);
     reporter.addHeaderColumn("Zahlungsgrund", Element.ALIGN_CENTER, 100,
         Color.LIGHT_GRAY);
-    reporter.addHeaderColumn("Zahlungsgrund2", Element.ALIGN_CENTER, 100,
+    reporter.addHeaderColumn("Buchungsart", Element.ALIGN_CENTER, 60,
         Color.LIGHT_GRAY);
     reporter.addHeaderColumn("Betrag", Element.ALIGN_CENTER, 50,
         Color.LIGHT_GRAY);
