11539ed1214848825eb5e759c9a5a192874463d4
jost <jost>
2009-11-19 21:11:43 +0000

    *** empty log message ***

--- jverein/web/changelog.php
+++ jverein/web/changelog.php
@@ -3,6 +3,7 @@
     <ul>
 	   	<li>Version 1.3 noch nicht freigegeben</li>
     		<ul>
+    			<li>Neu: Eigenschaften können jetzt in Eigenschaftengruppen zusammengefasst werden.</li>
     			<li>DB-Update restore: Meldung bei nicht leerer Datenbank</li>
     			<li>Mitglied: Anzeige IBAN</li>
     			<li>Neu: Import von Zusatzbeträgen. Sowohl Default-Format als auch individuelle Formate.</li>
@@ -443,6 +444,9 @@
 </ul>
     <!-- 
     $Log: changelog.php,v $
+    Revision 1.26  2009/11/19 21:11:43  jost
+    *** empty log message ***
+
     Revision 1.25  2009/11/17 21:05:36  jost
     *** empty log message ***
 
