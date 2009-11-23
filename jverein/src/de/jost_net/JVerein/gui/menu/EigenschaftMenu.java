cd217465090ca838e99f3c5130a3cc467c5cdb70
jost <jost>
2009-11-23 20:40:07 +0000

    Neuer Menüpunkt: neu

--- jverein/src/de/jost_net/JVerein/gui/menu/EigenschaftMenu.java
+++ jverein/src/de/jost_net/JVerein/gui/menu/EigenschaftMenu.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/menu/EigenschaftMenu.java,v $
- * $Revision: 1.1 $
- * $Date: 2009/11/17 20:58:32 $
+ * $Revision: 1.2 $
+ * $Date: 2009/11/23 20:40:17 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: EigenschaftMenu.java,v $
+ * Revision 1.2  2009/11/23 20:40:17  jost
+ * Neuer Menüpunkt: neu
+ *
  * Revision 1.1  2009/11/17 20:58:32  jost
  * Neu: Eigenschaft und EigenschaftGruppe
  *
@@ -17,8 +20,10 @@ package de.jost_net.JVerein.gui.menu;
 
 import de.jost_net.JVerein.JVereinPlugin;
 import de.jost_net.JVerein.gui.action.EigenschaftDeleteAction;
+import de.jost_net.JVerein.gui.action.EigenschaftDetailAction;
 import de.willuhn.jameica.gui.parts.CheckedContextMenuItem;
 import de.willuhn.jameica.gui.parts.ContextMenu;
+import de.willuhn.jameica.gui.parts.ContextMenuItem;
 
 /**
  * Kontext-Menu zur Eigenschaft.
@@ -31,6 +36,8 @@ public class EigenschaftMenu extends ContextMenu
    */
   public EigenschaftMenu()
   {
+    addItem(new ContextMenuItem(JVereinPlugin.getI18n().tr("neu"),
+        new EigenschaftDetailAction(true), "document-new.png"));
     addItem(new CheckedContextMenuItem(
         JVereinPlugin.getI18n().tr("löschen..."),
         new EigenschaftDeleteAction(), "user-trash.png"));
--- jverein/src/de/jost_net/JVerein/gui/view/EigenschaftGruppeListeView.java
+++ jverein/src/de/jost_net/JVerein/gui/view/EigenschaftGruppeListeView.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/view/EigenschaftGruppeListeView.java,v $
- * $Revision: 1.1 $
- * $Date: 2009/11/17 21:00:18 $
+ * $Revision: 1.2 $
+ * $Date: 2009/11/23 20:40:35 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: EigenschaftGruppeListeView.java,v $
+ * Revision 1.2  2009/11/23 20:40:35  jost
+ * Neuer Menüpunkt: neu
+ *
  * Revision 1.1  2009/11/17 21:00:18  jost
  * Neu: Eigenschaft und EigenschaftGruppe
  *
@@ -45,7 +48,8 @@ public class EigenschaftGruppeListeView extends AbstractView
         new EigenschaftGruppeDeleteAction(),
         control.getEigenschaftGruppeList(), false, "user-trash.png");
     buttons.addButton(JVereinPlugin.getI18n().tr("neu"),
-        new EigenschaftGruppeDetailAction(), null, false, "document-new.png");
+        new EigenschaftGruppeDetailAction(true), null, false,
+        "document-new.png");
   }
 
   public void unbind() throws ApplicationException
