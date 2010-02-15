7893db9c82aadb73a9909e3ffffe4cb7ddc47419
jost <jost>
2010-02-15 20:00:26 +0000

    *** empty log message ***

--- jverein/web/changelog.php
+++ jverein/web/changelog.php
@@ -3,6 +3,7 @@
     <ul>
 	   	<li>Version 1.3 noch nicht freigegeben</li>
     		<ul>
+    		    <li>Mailfunktion um Dateianhänge erweitert.</li>
     			<li>IBAN direkt bei der Eingabe von Kontonummer und BLZ berechnen</li>
     		    <li>Liste der Zusatzbeträge: true/false wird als X bzw. _ dargestellt</li>
     		    <li>Mitgliedsdaten: Der zuletzt ausgewählte Tab wird bei Betätigung von "zurück" aktiviert.</li>
@@ -459,6 +460,9 @@
 </ul>
     <!-- 
     $Log: changelog.php,v $
+    Revision 1.39  2010/02/15 20:00:26  jost
+    *** empty log message ***
+
     Revision 1.38  2010/02/15 19:55:39  jost
     *** empty log message ***
 
