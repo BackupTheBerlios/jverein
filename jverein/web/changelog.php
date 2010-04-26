1f78b7958dc09171d959576b2666998bcda79926
jost <jost>
2010-04-26 19:24:43 +0000

    *** empty log message ***

--- jverein/web/changelog.php
+++ jverein/web/changelog.php
@@ -5,6 +5,7 @@
     		<ul>
     			<li>Zusätzliche Prüfung der Bankverbindung bei der Abrechnung</li>
     			<li>Fehlendes Foreign-Key-Constraint Eigenschaften/Mitglied eingefügt.</li>
+				<li>Aktuelle Geburtstage: Korrekte Behandlung ausgetretener Mitglieder</li>
     		</ul>
 
 	   	<li>Version 1.3.0 vom 09.04.2010</li>
@@ -467,6 +468,9 @@
 </ul>
     <!-- 
     $Log: changelog.php,v $
+    Revision 1.46  2010/04/26 19:24:43  jost
+    *** empty log message ***
+
     Revision 1.45  2010/04/25 14:21:27  jost
     *** empty log message ***
 
