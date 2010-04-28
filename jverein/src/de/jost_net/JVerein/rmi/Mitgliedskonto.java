8faa3b22e6bb375a916c95f4b255ce3ff60d62c4
jost <jost>
2010-04-28 06:17:50 +0000

    Neu: Mitgliedskonto

---
+++ jverein/src/de/jost_net/JVerein/rmi/Mitgliedskonto.java
@@ -0,0 +1,55 @@
+/**********************************************************************
+ * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/rmi/Mitgliedskonto.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2010/04/28 06:17:50 $
+ * $Author: jost $
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * All rights reserved
+ * heiner@jverein.de
+ * www.jverein.de
+ * $Log: Mitgliedskonto.java,v $
+ * Revision 1.1  2010/04/28 06:17:50  jost
+ * Neu: Mitgliedskonto
+ *
+ **********************************************************************/
+package de.jost_net.JVerein.rmi;
+
+import java.rmi.RemoteException;
+import java.util.Date;
+
+import de.willuhn.datasource.rmi.DBObject;
+
+public interface Mitgliedskonto extends DBObject
+{
+  public String getID() throws RemoteException;
+
+  public Mitglied getMitglied() throws RemoteException;
+
+  public void setMitglied(Mitglied mitglied) throws RemoteException;
+
+  public String getBuchungstyp() throws RemoteException;
+
+  public void setBuchungstyp(String buchungstyp) throws RemoteException;
+
+  public Date getDatum() throws RemoteException;
+
+  public void setDatum(Date datum) throws RemoteException;
+
+  public String getZweck1() throws RemoteException;
+
+  public void setZweck1(String zweck1) throws RemoteException;
+
+  public String getZweck2() throws RemoteException;
+
+  public void setZweck2(String zweck2) throws RemoteException;
+
+  public void setBetrag(double betrag) throws RemoteException;
+
+  public double getBetrag() throws RemoteException;
+
+  public void setReferenz(String referenz) throws RemoteException;
+
+  public String getReferenz() throws RemoteException;
+
+}
