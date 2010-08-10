e5580dbac09199499d87410894e6ae156232bb9c
jost <jost>
2010-08-10 05:40:38 +0000

    Formularanzeige eingebaut

--- jverein/src/de/jost_net/JVerein/gui/view/FormularfelderListeView.java
+++ jverein/src/de/jost_net/JVerein/gui/view/FormularfelderListeView.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/view/FormularfelderListeView.java,v $
- * $Revision: 1.4 $
- * $Date: 2009/06/11 21:03:39 $
+ * $Revision: 1.5 $
+ * $Date: 2010/08/10 05:40:38 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: FormularfelderListeView.java,v $
+ * Revision 1.5  2010/08/10 05:40:38  jost
+ * Formularanzeige eingebaut
+ *
  * Revision 1.4  2009/06/11 21:03:39  jost
  * Vorbereitung I18N
  *
@@ -26,6 +29,7 @@ package de.jost_net.JVerein.gui.view;
 
 import de.jost_net.JVerein.JVereinPlugin;
 import de.jost_net.JVerein.gui.action.DokumentationAction;
+import de.jost_net.JVerein.gui.action.FormularAnzeigeAction;
 import de.jost_net.JVerein.gui.action.FormularfeldAction;
 import de.jost_net.JVerein.gui.control.FormularfeldControl;
 import de.jost_net.JVerein.rmi.Formular;
@@ -45,11 +49,14 @@ public class FormularfelderListeView extends AbstractView
         (Formular) getCurrentObject());
     control.getFormularfeldList().paint(this.getParent());
 
-    ButtonArea buttons = new ButtonArea(this.getParent(), 3);
+    ButtonArea buttons = new ButtonArea(this.getParent(), 4);
     buttons.addButton(new Back(false));
     buttons.addButton(JVereinPlugin.getI18n().tr("Hilfe"),
         new DokumentationAction(), DokumentationUtil.FORMULARE, false,
         "help-browser.png");
+    buttons.addButton(JVereinPlugin.getI18n().tr("anzeigen"),
+        new FormularAnzeigeAction(), (Formular) getCurrentObject(), false,
+        "edit.png");
     buttons.addButton(JVereinPlugin.getI18n().tr("neu"),
         new FormularfeldAction(), (Formular) getCurrentObject(), false,
         "document-new.png");
