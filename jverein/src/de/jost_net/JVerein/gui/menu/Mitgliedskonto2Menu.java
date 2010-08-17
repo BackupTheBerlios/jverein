5ce96a7247834d837d511eb3d0d8b998150cb0ef
jost <jost>
2010-08-17 18:36:28 +0000

    Kontextmenü für die Mahnung

--- jverein/src/de/jost_net/JVerein/gui/menu/Mitgliedskonto2Menu.java
+++ jverein/src/de/jost_net/JVerein/gui/menu/Mitgliedskonto2Menu.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/menu/Mitgliedskonto2Menu.java,v $
- * $Revision: 1.1 $
- * $Date: 2010/08/04 10:40:21 $
+ * $Revision: 1.2 $
+ * $Date: 2010/08/17 18:36:28 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,7 +9,10 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: Mitgliedskonto2Menu.java,v $
- * Revision 1.1  2010/08/04 10:40:21  jost
+ * Revision 1.2  2010/08/17 18:36:28  jost
+ * Kontextmenü für die Mahnung
+ *
+ * Revision 1.1  2010-08-04 10:40:21  jost
  * Prerelease Rechnung
  *
  **********************************************************************/
@@ -17,22 +20,26 @@
 package de.jost_net.JVerein.gui.menu;
 
 import de.jost_net.JVerein.JVereinPlugin;
+import de.jost_net.JVerein.gui.action.MitgliedskontoMahnungAction;
 import de.jost_net.JVerein.gui.action.MitgliedskontoRechnungAction;
 import de.willuhn.jameica.gui.parts.CheckedContextMenuItem;
 import de.willuhn.jameica.gui.parts.ContextMenu;
 
 /**
- * Kontext-Menu zu den Rechnungen.
+ * Kontext-Menu zu den Mitgliedskonten.
  */
 public class Mitgliedskonto2Menu extends ContextMenu
 {
 
   /**
-   * Erzeugt ein Kontext-Menu fuer die Liste der Rechnungen.
+   * Erzeugt ein Kontext-Menu fuer Mitgliedskonten.
    */
   public Mitgliedskonto2Menu()
   {
     addItem(new CheckedContextMenuItem(JVereinPlugin.getI18n()
         .tr("Rechnung..."), new MitgliedskontoRechnungAction(), "rechnung.png"));
+    addItem(new CheckedContextMenuItem(
+        JVereinPlugin.getI18n().tr("Mahnung..."),
+        new MitgliedskontoMahnungAction(), "rechnung.png"));
   }
 }
