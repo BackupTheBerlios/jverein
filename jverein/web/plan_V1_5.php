529b6d2041b2dcb1e84658fd577a2b13239bcf81
jost <jost>
2010-09-27 20:15:06 +0000

    *** empty log message ***

---
+++ jverein/web/plan_V1_5.php
@@ -0,0 +1,18 @@
+<? include ("frame.inc"); ?>
+    <h1>Plan für die Version 1.5</h1>
+    <p>
+    <p>Für die Version 1.5 sind bis jetzt folgende Erweiterungen geplant:</p>
+   
+   	<ul>
+   		<li> Verwaltung und Abrechnung von Arbeitsdiensten</li>
+   		<li> Filterung der Mitglieder auf die Zusatzfelder erweitern</li>
+   	</ul>
+    <!-- 
+    $Log: plan_V1_5.php,v $
+    Revision 1.1  2010/09/27 20:15:16  jost
+    *** empty log message ***
+
+    -->
+
+ <? include ("footer.inc"); ?>
+
