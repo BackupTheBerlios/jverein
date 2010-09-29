cd24b72aca0ebdb35e4fb60b1853db12f8bbcde4
jost <jost>
2010-09-29 16:38:12 +0000

    Button umbenannt.

--- jverein/src/de/jost_net/JVerein/gui/view/KontoListView.java
+++ jverein/src/de/jost_net/JVerein/gui/view/KontoListView.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/view/KontoListView.java,v $
- * $Revision: 1.8 $
- * $Date: 2010/08/23 13:39:32 $
+ * $Revision: 1.9 $
+ * $Date: 2010/09/29 16:38:12 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,7 +9,10 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: KontoListView.java,v $
- * Revision 1.8  2010/08/23 13:39:32  jost
+ * Revision 1.9  2010/09/29 16:38:12  jost
+ * Button umbenannt.
+ *
+ * Revision 1.8  2010-08-23 13:39:32  jost
  * Optimierung Tastatursteuerung
  *
  * Revision 1.7  2009/06/11 21:03:39  jost
@@ -62,7 +65,7 @@ public class KontoListView extends AbstractView
     buttons.addButton(JVereinPlugin.getI18n().tr("&Hilfe"),
         new DokumentationAction(), DokumentationUtil.KONTEN, false,
         "help-browser.png");
-    buttons.addButton(JVereinPlugin.getI18n().tr("Hibiscus-&Import"),
+    buttons.addButton(JVereinPlugin.getI18n().tr("Hibiscus-Konten-&Import"),
         new HibiscusKontenImportAction(control), null, false, "go.png");
     buttons.addButton(JVereinPlugin.getI18n().tr("&neu"), new KontoAction(),
         null, false, "document-new.png");
