ff517363b90a3e58bd7bbbabd13fe51748e6e5ed
jost <jost>
2011-02-03 22:32:24 +0000

    Neu: Liste der Buchungsarten

--- jverein/src/de/jost_net/JVerein/gui/view/BuchungsartListView.java
+++ jverein/src/de/jost_net/JVerein/gui/view/BuchungsartListView.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/view/BuchungsartListView.java,v $
- * $Revision: 1.12 $
- * $Date: 2011/01/15 09:46:49 $
+ * $Revision: 1.13 $
+ * $Date: 2011/02/03 22:32:39 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,7 +9,10 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: BuchungsartListView.java,v $
- * Revision 1.12  2011/01/15 09:46:49  jost
+ * Revision 1.13  2011/02/03 22:32:39  jost
+ * Neu: Liste der Buchungsarten
+ *
+ * Revision 1.12  2011-01-15 09:46:49  jost
  * Tastatursteuerung wegen Problemen mit Jameica/Hibiscus wieder entfernt.
  *
  * Revision 1.11  2010-10-15 09:58:25  jost
@@ -66,12 +69,12 @@ public class BuchungsartListView extends AbstractView
 
     control.getBuchungsartList().paint(this.getParent());
 
-    ButtonArea buttons = new ButtonArea(this.getParent(), 3);
+    ButtonArea buttons = new ButtonArea(this.getParent(), 4);
     buttons.addButton(new Back(false));
     buttons.addButton(JVereinPlugin.getI18n().tr("Hilfe"),
         new DokumentationAction(), DokumentationUtil.BUCHUNGSARTEN, false,
         "help-browser.png");
-
+    buttons.addButton(control.getPDFAusgabeButton());
     buttons.addButton(JVereinPlugin.getI18n().tr("neu"),
         new BuchungsartAction(), null, false, "document-new.png");
   }
