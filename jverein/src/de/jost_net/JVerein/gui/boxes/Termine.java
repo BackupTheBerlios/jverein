1a31730f6f08f90115b804a3aae96b0c7c1a89b5
jost <jost>
2011-03-18 19:15:03 +0000

    Neu: JVerein-Termine auf der Startseite

---
+++ jverein/src/de/jost_net/JVerein/gui/boxes/Termine.java
@@ -0,0 +1,80 @@
+/**********************************************************************
+ * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/boxes/Attic/Termine.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2011/03/18 19:15:03 $
+ * $Author: jost $
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * All rights reserved
+ * heiner@jverein.de
+ * www.jverein.de
+ * $Log: Termine.java,v $
+ * Revision 1.1  2011/03/18 19:15:03  jost
+ * Neu: JVerein-Termine auf der Startseite
+ *
+ **********************************************************************/
+package de.jost_net.JVerein.gui.boxes;
+
+import java.rmi.RemoteException;
+import java.util.Date;
+
+import org.eclipse.swt.widgets.Composite;
+
+import de.jost_net.JVerein.JVereinPlugin;
+import de.jost_net.JVerein.gui.parts.TerminePart;
+import de.jost_net.JVerein.gui.view.TermineView;
+import de.willuhn.jameica.gui.boxes.AbstractBox;
+import de.willuhn.jameica.gui.boxes.Box;
+import de.willuhn.jameica.gui.internal.buttons.Back;
+import de.willuhn.jameica.gui.parts.ButtonArea;
+import de.willuhn.util.I18N;
+
+public class Termine extends AbstractBox implements Box
+{
+  private TerminePart termine = null;
+
+  private final static I18N i18n = JVereinPlugin.getI18n();
+
+  private static Date currentDate = null;
+
+  public Termine()
+  {
+    super();
+    currentDate = new Date();
+  }
+
+  public String getName()
+  {
+    return JVereinPlugin.getI18n().tr("JVerein: Termine");
+  }
+
+  public int getDefaultIndex()
+  {
+    return 3;
+  }
+
+  public boolean getDefaultEnabled()
+  {
+    return false;
+  }
+
+  public void paint(Composite parent) throws RemoteException
+  {
+    this.termine = new TerminePart();
+    this.termine.setCurrentDate(currentDate);
+    this.termine.paint(parent);
+  }
+
+  @Override
+  public boolean isActive()
+  {
+    return super.isActive();
+  }
+
+  @Override
+  public int getHeight()
+  {
+    return 500;
+  }
+
+}
