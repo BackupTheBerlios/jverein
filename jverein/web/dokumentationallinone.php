49563cf7d8303bc0d3ab2d108e39a3a495311f45
jost <jost>
2011-04-17 17:56:45 +0000

    *** empty log message ***

--- jverein/web/dokumentationallinone.php
+++ jverein/web/dokumentationallinone.php
@@ -15,7 +15,7 @@
 	<? include ("kursteilnehmer.php"); ?>
 	<? include ("abrechnung.php"); ?>
 	<? include ("zusatzbetraege.php"); ?>
-	<? include ("zusatzbetraegeimport.php"); ?>
+	<? include ("zusatzbetraege_import.php"); ?>
 	<? include ("manuelle_zahlungseingaenge.php"); ?>
 	<? include ("wiedervorlage.php"); ?>
 	<? include ("lehrgaenge.php"); ?>
@@ -43,7 +43,10 @@
 
     <!-- 
     $Log: dokumentationallinone.php,v $
-    Revision 1.9  2010/11/03 11:57:37  jost
+    Revision 1.10  2011/04/17 17:56:45  jost
+    *** empty log message ***
+
+    Revision 1.9  2010-11-03 11:57:37  jost
     *** empty log message ***
 
     Revision 1.8  2010-03-05 21:55:46  jost
