ae28e22df3562b441c1e5c8573f2de9cca6ec39a
jost <jost>
2011-06-20 15:11:57 +0000

    Bei der automatischen Erstellung von Spendenbescheinigungen wird das Formular mit vorgegeben.

--- jverein/src/de/jost_net/JVerein/gui/view/SpendenbescheinigungAutoNeuView.java
+++ jverein/src/de/jost_net/JVerein/gui/view/SpendenbescheinigungAutoNeuView.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/view/SpendenbescheinigungAutoNeuView.java,v $
- * $Revision: 1.3 $
- * $Date: 2011/04/07 19:35:47 $
+ * $Revision: 1.4 $
+ * $Date: 2011/06/20 15:12:20 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,7 +9,10 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log: SpendenbescheinigungAutoNeuView.java,v $
- * Revision 1.3  2011/04/07 19:35:47  jost
+ * Revision 1.4  2011/06/20 15:12:20  jost
+ * Bei der automatischen Erstellung von Spendenbescheinigungen wird das Formular mit vorgegeben.
+ *
+ * Revision 1.3  2011-04-07 19:35:47  jost
  * Neue Zurückbutton-Mimik aus Jameica
  *
  * Revision 1.2  2011-03-09 22:16:41  jost
@@ -26,7 +29,7 @@ import de.jost_net.JVerein.gui.action.DokumentationAction;
 import de.jost_net.JVerein.gui.control.SpendenbescheinigungAutoNeuControl;
 import de.willuhn.jameica.gui.AbstractView;
 import de.willuhn.jameica.gui.GUI;
-import de.willuhn.jameica.gui.util.ButtonArea;
+import de.willuhn.jameica.gui.parts.ButtonArea;
 import de.willuhn.jameica.gui.util.LabelGroup;
 
 public class SpendenbescheinigungAutoNeuView extends AbstractView
@@ -45,14 +48,16 @@ public class SpendenbescheinigungAutoNeuView extends AbstractView
     LabelGroup group = new LabelGroup(getParent(), JVereinPlugin.getI18n().tr(
         "Jahr"));
     group.addLabelPair("Jahr", control.getJahr());
+    group.addLabelPair("Formular", control.getFormular());
 
     control.getSpendenbescheinigungTree().paint(this.getParent());
 
-    ButtonArea buttons = new ButtonArea(this.getParent(), 2);
+    ButtonArea buttons = new ButtonArea();
     buttons.addButton(control.getSpendenbescheinigungErstellenButton());
     buttons.addButton(JVereinPlugin.getI18n().tr("Hilfe"),
         new DokumentationAction(), DokumentationUtil.SPENDENBESCHEINIGUNG,
         false, "help-browser.png");
+    buttons.paint(getParent());
   }
 
   @Override
