9da741fc255a303e61f13811f6a04c7dd7200bb5
jost <jost>
2011-07-24 18:02:54 +0000

    Neu: Spenden für JVerein

---
+++ jverein/src/de/jost_net/JVerein/gui/action/SpendenAction.java
@@ -0,0 +1,48 @@
+/**********************************************************************
+ei * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/gui/action/SpendenAction.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2011/07/24 18:02:54 $
+ * $Author: jost $
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * All rights reserved
+ * heiner@jverein.de
+ * www.jverein.de
+ * $Log: SpendenAction.java,v $
+ * Revision 1.1  2011/07/24 18:02:54  jost
+ * Neu: Spenden für JVerein
+ *
+ **********************************************************************/
+
+package de.jost_net.JVerein.gui.action;
+
+import de.jost_net.JVerein.gui.view.SpendenView;
+import de.willuhn.jameica.gui.Action;
+import de.willuhn.jameica.gui.GUI;
+import de.willuhn.util.ApplicationException;
+
+/**
+ * Action zum Starten der Spenden-View.
+ */
+public class SpendenAction implements Action
+{
+
+  /**
+   * @see de.willuhn.jameica.gui.Action#handleAction(java.lang.Object)
+   */
+  public void handleAction(Object context) throws ApplicationException
+  {
+    GUI.startView(SpendenView.class, null);
+  }
+
+}
+
+/**********************************************************************
+ * $Log: SpendenAction.java,v $
+ * Revision 1.1  2011/07/24 18:02:54  jost
+ * Neu: Spenden für JVerein
+ * Revision 1.1 2010-08-20 12:42:02 willuhn
+ * 
+ * @N Neuer Spenden-Aufruf. Ich bin gespannt, ob das klappt ;)
+ * 
+ **********************************************************************/
