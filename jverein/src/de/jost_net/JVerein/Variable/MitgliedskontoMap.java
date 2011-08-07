d2ce8e6721140075ec65e9dd3f4cc13b7e068491
jost <jost>
2011-08-07 08:17:48 +0000

    Bugfix neue Datenfelder

--- jverein/src/de/jost_net/JVerein/Variable/MitgliedskontoMap.java
+++ jverein/src/de/jost_net/JVerein/Variable/MitgliedskontoMap.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/jverein/Repository/jverein/src/de/jost_net/JVerein/Variable/MitgliedskontoMap.java,v $
- * $Revision: 1.1 $
- * $Date: 2011/05/27 18:48:45 $
+ * $Revision: 1.2 $
+ * $Date: 2011/08/07 08:17:48 $
  * $Author: jost $
  *
  * Copyright (c) by Heiner Jostkleigrewe
@@ -9,7 +9,10 @@
  * www.jverein.de
  * All rights reserved
  * $Log: MitgliedskontoMap.java,v $
- * Revision 1.1  2011/05/27 18:48:45  jost
+ * Revision 1.2  2011/08/07 08:17:48  jost
+ * Bugfix neue Datenfelder
+ *
+ * Revision 1.1  2011-05-27 18:48:45  jost
  * Vereinheitlichung Variable
  *
  **********************************************************************/
@@ -44,7 +47,7 @@ public class MitgliedskontoMap
     {
       map = inma;
     }
-    
+
     ArrayList<Date> buda = new ArrayList<Date>();
     ArrayList<String> zg = new ArrayList<String>();
     ArrayList<String> zg1 = new ArrayList<String>();
@@ -71,7 +74,12 @@ public class MitgliedskontoMap
     map.put(FormularfeldControl.ZAHLUNGSGRUND1, zg1.toArray());
     map.put(FormularfeldControl.ZAHLUNGSGRUND2, zg2.toArray());
     map.put(FormularfeldControl.BETRAG, betrag.toArray());
-    
+    map.put(MitgliedskontoVar.BUCHUNGSDATUM.getName(), buda.toArray());
+    map.put(MitgliedskontoVar.ZAHLUNGSGRUND.getName(), zg.toArray());
+    map.put(MitgliedskontoVar.ZAHLUNGSGRUND1.getName(), zg1.toArray());
+    map.put(MitgliedskontoVar.ZAHLUNGSGRUND2.getName(), zg2.toArray());
+    map.put(MitgliedskontoVar.BETRAG.getName(), betrag.toArray());
+
     return map;
   }
 }
